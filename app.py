a078093ce1cbe5f9875da9686c524d17556260c9
Kev <kevin.saner91@hotmail.com>
2023-02-20 11:45:11 +0100

    finished runner

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, jsonify, session, request, url_for
+from flask import Flask, render_template, jsonify, session, request, redirect
 import qrcode
 import requests
 import json
+import time
 
 
 app = Flask(__name__)
@@ -38,14 +39,52 @@ def check_connection():
 @app.route('/name', methods=['POST', 'GET'])      
 def name():
     if request.method == 'POST':
-        #issue the credential
-        pass
+        url = 'http://localhost:8100/issue/process'
+        data = {
+            "connectionId": session['connection'],
+            "credentialDefinitionId": "E8sTdcBe7fue6eFaAi231P:3:CL:128085:1.0",
+            "attributes": {
+                "score": "99"
+            },
+            "userId": "Anonymous"
+        }
+        print(data)
+
+        headers = {"Content-Type": "application/json"}
+
+        response = requests.post(
+            url,
+            json=data,
+            headers=headers)
+
+        if response.status_code == 200:
+            data = json.loads(response.text)
+            session['processId'] = data['processId']
+
+            return render_template('loading.html')
+
+        else:
+            return render_template('failure.html')    
     else:
         #just show the page
         return render_template('name.html')
 
-    
 
+@app.route('/loading/')
+def loading():
+    # Check the Acception status by making a GET request to the API endpoint
+    url = 'http://localhost:8100/issue/process/' + session['processId'] + '/state'
+    response = requests.get(url)
+    if response.text != '"IN_PROGRESS"':
+        # Credential has been accepted
+        return jsonify({'status': 'accepted'})
+    else:
+        # Credential has not been accepted
+        return jsonify({'status': 'not accepted'})
+
+@app.route('/success')
+def success():
+    return render_template('success.html')        
   
 
 if __name__ == "__main__":
